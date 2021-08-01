4dae946481e70b2a9ab82ac1d336fc9c72372280
Abdallah-Abu-Hussein <abdallh1133224466@gmail.com>
2021-08-02 02:37:04 +0300

    a lot of things

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 alert("Welocme to my website");
+
 let  name = prompt("We need to konw your beautiful Name, for better conversation");
 alert("good to meet you! " + name) ;
-console.log("user_name :"+name);
-let fav_food = prompt("What's your favorite favorite food in those three jordanian dishes\n  \n Mansaf\n Kanafeh \n Falafel ")
+console.log("user_name is :"+name);
+
+document.write('<p style="font-size : 35px ;background-color: #D4F1F4; color: green">'+ 'Your name is : '+ name +'</p>')
+
+let age = prompt("what is your age?");
+    console.log('asking user for his age = ' + age)
+    alert(age + ' years');
+    document.write('<p style="font-size : 35px ;background-color: red; color: lightgreen">'+ 'Your Age is : '+ age +'</p>')
+
+
+let interseted_in_jordan = prompt("Are you welling to come to jordan to eat our beloved dishes , type y/yes or n/no")
+
+if (interseted_in_jordan == "y") {
+    alert( "can't wait to have ")
+    console.log("the user interseted_in_jordan")
+    document.write('<p style = " color: #81B622;background-color:#189AB4; font-size : 35px ;"> ' +name +'is interseted_in_jordan'+ '</p>');
+}else{
+    document.write('<p style = " color: #3D550C;background-color: #189AB4; font-size : 35px ;"> ' +name +'is Not interseted_in_jordan'+ '</p>');
+}
+
+
+
+let fav_food = prompt("What's your favorite food in those three jordanian dishes\n  \n Mansaf\n Kanafeh \n Falafel ")
 
 if (fav_food == 'Mansaf') {
    alert('You have graet taste , nice !!!')
-   alert("let's to Know more about this dish ")
-     document.write('<img src="https://kitchen.sayidaty.net/uploads/small/43/433e4b0fab1635469b33b63d17be6bb4_w750_h500.jpg" width="1000" height= "400"/>'+'<p style="background-color:red; color: lightgreen">'+' The original pastoralist Bedouin mansaf underwent significant changes in the 20th century. The dish is said to originally have been made with simply meat (camel or lamb), meat broth or ghee (clarified butter) and bread.[4] Following the popularization of rice in northern Jordan in the 1920s, rice gradually was introduced into the dish, at first mixed with bulgur, and later on its own, until the dish reached its modern incarnation of being based on white rice. Similarly, the jameed sauce is a recent development, as the Bedouins did not historically feature jameed in their cooked dishes until their modern sedentarization . ' + '</p>')
+   console.log("fav_food = " + fav_food);
+     document.write('<p style="background-color:red; color: lightgreen; font-size : 35px;">'+ 'your favorite food is  :'+fav_food+'</p>')
+
 } else if(fav_food == 'Kanafeh') {
+    console.log("fav_food = " + fav_food);
     alert('You have graet taste , nice !!!')
-    alert("let's to Know more about this dish ")
-    document.write('<img  src="https://alkaramahdough.com/wp-content/uploads/2019/09/grounded-kunafa5.png" width="1000" height= "400"/> '+'<p style =" background-color: black; color: #FFFF00; ">'+ "Kanafeh is a traditional Middle Eastern dessert made with shredded filo pastry, or alternatively fine semolina dough, soaked in sweet, sugar-based syrup, and typically layered with cheese, or with other ingredients such as clotted cream or nuts, depending on the region." +'</p>')
+    document.write('<p style="background-color:yellow; color: green; font-size : 35px ;">' + 'Your favorite food is :'+fav_food+'</p>')
+
 } else if(fav_food == 'Falafel'){
+    console.log("fav_food = " + fav_food);
     alert('You have graet taste , nice !!!')
-    alert("let's to Know more about this dish ")
-    document.write('<img src="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/07/Falafel-7.jpg" width="1000" height= "400"/> ' + '<p style="background-color:blue;color:red;"> Falafel, a staple Middle Eastern dish—and a popular street food around the world—that consists of fried spiced balls or patties of ground chickpeas or fava beans (or a mixture of both) stuffed into a pita or wrapped in laffa bread with hot sauce, tahini sauce, and generally some saladlike combination of tomato, lettuce, cucumber, onion, parsley, and yogurt.</p>')
+    document.write('<img src="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/07/Falafel-7.jpg" width="1000" height= "400"/> ' + '<p style ="background-color:blue;color:red;font-size : 35px ;">' +'Your favorite fodd is fav_food : '+fav_food+'</p>')
+ 
+
 }else{
     alert('Error,invald input')
 }
+
